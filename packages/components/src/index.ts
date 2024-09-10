75c034ff6f67210aa328fce368d9ef3df26ad035
HJiaxing <17388766232@163.com>
2024-09-10 20:36:22 +0800

    ✅测试: 版本变更测试

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -2,4 +2,6 @@ import BaseForm from "./BaseForm";
 import BaseModal from "./BaseModal";
 import BaseTable from "./BaseTable";
 
+console.log("BaseForm,发布测试");
+
 export { BaseForm, BaseModal, BaseTable };
