c384e667b841c471e6d20815d6327a825f3f64db
HJiaxing <17388766232@163.com>
2024-09-29 09:33:56 +0800

    🔧工具: 打包工具优化

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -1,23 +1,67 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      tsconfigPath: "./tsconfig.prod.json",
+      outDir: "build/lib",
+    }),
+    dts({
+      tsconfigPath: "./tsconfig.prod.json",
+      outDir: "build/es",
+    }),
+  ],
   build: {
+    outDir: "build",
+    cssCodeSplit: true,
     lib: {
       entry: "src/index.ts",
       name: "components",
-      fileName: (format) => `${format}/index.js`,
+      fileName: (format) => `components.${format}/index.js`,
     },
     rollupOptions: {
       external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+      output: [
+        {
+          format: "es",
+          entryFileNames: "[name].js",
+          exports: "named",
+          name: "components",
+          dir: "./build/dist",
+          globals: {
+            react: "React",
+            "react-dom": "ReactDOM",
+          },
         },
-      },
+        {
+          format: "es",
+          entryFileNames: "[name].js",
+          exports: "named",
+          preserveModules: true,
+          preserveModulesRoot: "src",
+          dir: "./build/es",
+          globals: {
+            react: "React",
+            "react-dom": "ReactDOM",
+          },
+        },
+        {
+          format: "cjs",
+          entryFileNames: "[name].js",
+          exports: "named",
+          preserveModules: true,
+          preserveModulesRoot: "src",
+          dir: "./build/lib",
+          globals: {
+            react: "React",
+            "react-dom": "ReactDOM",
+          },
+        },
+      ],
     },
   },
 });
